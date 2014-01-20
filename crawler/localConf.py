bacd217c1c3893ca241d9afb0997f4ac642e3e8f
Roohy <live@roohy.me>
2014-01-21 03:01:20 +0330

    fapfap

--- crawler/localConf.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'roohy'
-
-JARFILELOCATION = '/home/roohy/Desktop/haha.jar'
-
-INDEX_DIR = '/home/roohy/PycharmProjects/MirFinal/indexDIR'
-
-JSON_DIR = "/home/roohy/PycharmProjects/MirFinal/tempFile"
